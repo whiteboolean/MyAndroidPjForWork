74e2975be14d1d634c49266d19006633b58cb4e3
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-09-04 15:42:12 +0800

    自定义TabLayoutIndicator

--- app/src/main/java/com/example/myandroidpjforwork/MainActivity.kt
+++ app/src/main/java/com/example/myandroidpjforwork/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.myandroidpjforwork
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TabHost
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myandroidpjforwork.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+
     }
+
+
 }
