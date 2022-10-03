0eba89f93369e6b6938cdfdc9aecde25201a3434
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-10-03 22:02:50 +0800

    https://www.jianshu.com/p/568e3c22e9db
    ConstraintLayout的各种用法

--- app/src/main/java/com/example/myandroidpjforwork/ui/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myandroidpjforwork/ui/ui/dashboard/DashboardFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myandroidpjforwork.databinding.FragmentDashboardBinding
@@ -34,10 +35,11 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.layer.setOnClickListener {
+            Toast.makeText(requireContext(), "点击了layer", Toast.LENGTH_SHORT).show()
         }
+
+
         return root
     }
 
