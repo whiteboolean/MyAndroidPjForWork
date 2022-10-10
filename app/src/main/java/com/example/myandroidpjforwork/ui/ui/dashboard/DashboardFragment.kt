0148d291c9f4261811ae54edfeb6a27a53c993a7
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-10-10 21:46:14 +0800

    异步加载布局

--- app/src/main/java/com/example/myandroidpjforwork/ui/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myandroidpjforwork/ui/ui/dashboard/DashboardFragment.kt
@@ -4,47 +4,86 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ProgressBar
+import androidx.asynclayoutinflater.view.AsyncLayoutInflater
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.myandroidpjforwork.databinding.FragmentDashboardBinding
-import com.google.android.material.transition.MaterialContainerTransform
+import com.example.myandroidpjforwork.R
+//import com.example.myandroidpjforwork.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
-
-    private var _binding: FragmentDashboardBinding? = null
+//
+//    private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+//    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        sharedElementEnterTransition = MaterialContainerTransform()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        val dashboardViewModel =
+//            ViewModelProvider(this).get(DashboardViewModel::class.java)
+//
+//        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
+//
+////        new AsyncLayoutInflater(this).inflate(R.layout.activity_main, null, new AsyncLayoutInflater.OnInflateFinishedListener() {
+////        @Override public void onInflateFinished(@NonNull View view, int resid, @Nullable ViewGroup parent)
+////        { setContentView(view); //...... } });
+//
+//        AsyncLayoutInflater(requireContext()).inflate(R.layout.fragment_dashboard,null,object:AsyncLayoutInflater.OnInflateFinishedListener{
+//            override fun onInflateFinished(view: View, resid: Int, parent: ViewGroup?) {
+//
+//            }
+//
+//        })
+//
+//
+//        binding.layer.setOnClickListener {
+//            Toast.makeText(requireContext(), "点击了layer", Toast.LENGTH_SHORT).show()
+//        }
+//
+//
+//        return root
+//    }
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
 
-        binding.layer.setOnClickListener {
-            Toast.makeText(requireContext(), "点击了layer", Toast.LENGTH_SHORT).show()
+        // inflate loading view
+        val final_view = inflater.inflate(R.layout.fragment_loading_view, container, false)
+        // get progressbar
+        var progressBar = final_view.findViewById<ProgressBar>(R.id.cpb)
+        // initialize async layout inflater
+        val asyncLayoutInflater = context?.let { AsyncLayoutInflater(it) }
+        // inflate other layout
+        asyncLayoutInflater?.inflate(R.layout.fragment_dashboard_layout, null) { view, resid, parent ->
+            // when layout loaded show...
+            progressBar.postDelayed({
+                progressBar.visibility = View.GONE
+                (final_view as? ViewGroup)?.addView(view) // add view to already inflated view
+
+
+
+
+            },300)
+
         }
 
-
-        return root
+        return final_view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+//        _binding = null
     }
 }
