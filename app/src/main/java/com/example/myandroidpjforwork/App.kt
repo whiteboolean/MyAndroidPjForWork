9e9a90558b7415d66695e9103b4f5fe26f4ebe69
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-11-08 22:48:35 +0800

    引入依赖注入框架koin

--- app/src/main/java/com/example/myandroidpjforwork/App.kt
+++ app/src/main/java/com/example/myandroidpjforwork/App.kt
@@ -3,6 +3,9 @@ package com.example.myandroidpjforwork
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class App : Application() {
 
@@ -14,6 +17,13 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+
+        //注册koin
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules()
+        }
     }
 
 }
