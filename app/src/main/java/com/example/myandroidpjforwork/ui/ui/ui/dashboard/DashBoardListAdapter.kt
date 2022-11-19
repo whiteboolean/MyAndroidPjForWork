74476bec926ce7506298f58354c9dcd17fff249d
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-11-19 22:34:29 +0800

    根据滚动条件刷新列表

--- app/src/main/java/com/example/myandroidpjforwork/ui/ui/ui/dashboard/DashBoardListAdapter.kt
+++ app/src/main/java/com/example/myandroidpjforwork/ui/ui/ui/dashboard/DashBoardListAdapter.kt
@@ -5,12 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.myandroidpjforwork.databinding.ItemRcvBinding
 
-class DashBoardListAdapter( var list: MutableList<String>) :
+class DashBoardListAdapter(var list: MutableList<String>) :
     RecyclerView.Adapter<DashBoardListAdapter.ViewHolder>() {
-    private  val TAG = "DashBoardListAdapter"
+    private val TAG = "DashBoardListAdapter"
 
 //    //ViewHolder类将子项布局中所有控件绑定为一个对象，该对象包含子项布局的所有控件
 //    static class ViewHolder extends RecyclerView.ViewHolder {
@@ -26,16 +25,30 @@ class DashBoardListAdapter( var list: MutableList<String>) :
 //            Log.d("TextAdpter:","ViewHolder:" + anInt);
 //        }
 
+    /**
+     * 默认滑动状态
+     */
+    private var scrollState: Int = 0
+
+    fun setScrollState(scrollState: Int) {
+        this.scrollState = scrollState
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         Log.d(TAG, "onCreateViewHolder: ")
         return ViewHolder(
-            ItemRcvBinding.inflate(LayoutInflater.from(parent.context),parent,false).root
+            ItemRcvBinding.inflate(LayoutInflater.from(parent.context), parent, false).root
         )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Log.d(TAG, "onBindViewHolder: ")
+        Log.d(TAG, "onBindViewHolder: scrollState:${scrollState}")
+        if (scrollState != 0) {
+            return
+        }
+        Log.d(TAG, "疯狂刷新页面刷新页面")
+
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/myandroidpjforwork/ui/ui/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myandroidpjforwork/ui/ui/ui/dashboard/DashboardFragment.kt
@@ -7,13 +7,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myandroidpjforwork.databinding.FragmentDashboardBinding
 import com.example.myandroidpjforwork.ui.MainActivity
+import java.util.Timer
+import kotlin.concurrent.timerTask
 
 class DashboardFragment : Fragment() {
 
@@ -25,24 +26,65 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var rcvAdapter: DashBoardListAdapter
+
+
+    //停止
+    val SCROLL_STATE_IDLE = 0
+
+    //正在被外部拖拽,一般为用户正在用手指滚动
+    val SCROLL_STATE_DRAGGING = 1
+
+    //自动滚动开始
+    val SCROLL_STATE_SETTLING = 2
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         Log.d(TAG, "onCreateView: ")
-        binding.recyclerView.apply {
-            adapter = DashBoardListAdapter(dashboardViewModel.list.value ?: mutableListOf())
-            layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.let {
+            rcvAdapter = DashBoardListAdapter(dashboardViewModel.list.value ?: mutableListOf())
+            it.adapter = rcvAdapter
+            it.layoutManager = LinearLayoutManager(context)
+            it.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    println("newState:${newState}")
+                    when (newState) {
+                        SCROLL_STATE_IDLE -> {
+                            rcvAdapter.setScrollState(SCROLL_STATE_IDLE)
+                        }
+
+                        SCROLL_STATE_DRAGGING -> {
+                            rcvAdapter.setScrollState(SCROLL_STATE_DRAGGING)
+                        }
+
+                        SCROLL_STATE_SETTLING -> {
+                            rcvAdapter.setScrollState(SCROLL_STATE_SETTLING)
+                        }
+                    }
+                }
+            })
+
+            val timerTask = timerTask {
+                activity?.runOnUiThread {
+                    rcvAdapter.notifyDataSetChanged()
+                }
+            }
+            val timer = Timer()
+            timer.schedule(timerTask, 0, 1000)
         }
 
 
+
+
         binding.button4.setOnClickListener {
-            startActivity(Intent(activity,MainActivity::class.java))
+            startActivity(Intent(activity, MainActivity::class.java))
         }
 
 
@@ -64,8 +106,8 @@ class DashboardFragment : Fragment() {
         super.onPause()
         Log.d(TAG, "onPause: ")
 
-        ( binding.recyclerView.adapter as DashBoardListAdapter ).list  = mutableListOf()
-        (binding.recyclerView.adapter as DashBoardListAdapter).notifyDataSetChanged()
+//        (binding.recyclerView.adapter as DashBoardListAdapter).list = mutableListOf()
+//        (binding.recyclerView.adapter as DashBoardListAdapter).notifyDataSetChanged()
 
 
     }
@@ -89,7 +131,7 @@ class DashboardFragment : Fragment() {
         super.onDetach()
         Log.d(TAG, "onDetach: ")
     }
-    
+
     override fun onDestroyView() {
         super.onDestroyView()
         Log.d(TAG, "onDestroyView: ")
